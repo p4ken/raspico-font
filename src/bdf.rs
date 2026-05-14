243315cc9162b2fc45cde61d75da87f020f5b7f4
p4ken <commit@p4ken.skr.jp>
2026-05-14 21:52:49 +0900

    Refactor adding struct Font

--- src/bdf.rs
+++ src/bdf.rs
@@ -3,26 +3,45 @@ use anyhow::Context;
 // bdf_reader はMSX_FONTで様々なエラーが出て手に負えなかったので、手実装。
 // 例えば METRICSSET 2 がエラーを出す。
 pub fn convert(bdf: &[u8]) -> anyhow::Result<Vec<u8>> {
-    let lines = std::str::from_utf8(bdf)
-        .context("utf8")?
-        .lines()
-        .collect::<Vec<_>>();
-    anyhow::ensure!(lines.first().is_some_and(|l| l.starts_with("STARTFONT ")));
+    Font::from_bdf(bdf).context("parse")?.to_p4()
+}
 
-    let mut split_lines = lines
-        .strip_suffix(&["ENDFONT"])
-        .context("no ENDFONT")?
-        .splitn(2, |l| l.starts_with("CHARS "));
-    let header = Header::from_lines(split_lines.next().unwrap())?;
-    let chars_lines = split_lines.next().context("no CHARS")?;
+struct Font {
+    header: Header,
+    glyphs: Vec<Glyph>,
+}
 
-    let mut artifact = Vec::new();
-    for glyph_lines in chars_lines.split_inclusive(|l| *l == "ENDCHAR") {
-        let glyph = Glyph::from_lines(glyph_lines, &header).context(glyph_lines.join("\n"))?;
-        artifact.extend(glyph.bitmap);
+impl Font {
+    fn from_bdf(bdf: &[u8]) -> anyhow::Result<Self> {
+        let lines = std::str::from_utf8(bdf)
+            .context("utf8")?
+            .lines()
+            .collect::<Vec<_>>();
+        anyhow::ensure!(lines.first().is_some_and(|l| l.starts_with("STARTFONT ")));
+
+        let mut split_lines = lines
+            .strip_suffix(&["ENDFONT"])
+            .context("no ENDFONT")?
+            .splitn(2, |l| l.starts_with("CHARS "));
+        let header = Header::from_lines(split_lines.next().unwrap())?;
+        let chars_lines = split_lines.next().context("no CHARS")?;
+
+        let mut glyphs = Vec::new();
+        for glyph_lines in chars_lines.split_inclusive(|l| *l == "ENDCHAR") {
+            let glyph = Glyph::from_lines(glyph_lines).context("glyph")?;
+            glyphs.push(glyph);
+        }
+
+        Ok(Self { header, glyphs })
     }
 
-    Ok(artifact)
+    fn to_p4(&self) -> anyhow::Result<Vec<u8>> {
+        let mut out = Vec::new();
+        for glyph in &self.glyphs {
+            out.extend(glyph.to_p4(&self.header)?);
+        }
+        Ok(out)
+    }
 }
 
 struct Header {
@@ -49,11 +68,11 @@ impl Header {
 #[derive(Default)]
 struct Glyph {
     encoding: u16,
-    bitmap: Vec<u8>,
+    bitmap: Vec<[u8; 4]>,
 }
 
 impl Glyph {
-    fn from_lines(lines: &[&str], header: &Header) -> anyhow::Result<Self> {
+    fn from_lines(lines: &[&str]) -> anyhow::Result<Self> {
         anyhow::ensure!(lines.first().is_some_and(|l| l.starts_with("STARTCHAR ")));
         let lines = lines.strip_suffix(&["ENDCHAR"]).context("no ENDCHAR")?;
         let mut split_lines = lines.splitn(2, |l| *l == "BITMAP");
@@ -72,11 +91,19 @@ impl Glyph {
             return Ok(Glyph::default());
         }
 
-        let line_byte_offset = 4 - (header.width / 8);
         let mut bitmap = Vec::new();
         for line in bitmap_lines {
             // hex表現の文字列 → 0,1のビット列
-            let line_bytes = u32::from_str_radix(*line, 16).context("hex")?.to_be_bytes();
+            bitmap.push(u32::from_str_radix(*line, 16).context("hex")?.to_be_bytes());
+        }
+
+        Ok(Glyph { encoding, bitmap })
+    }
+
+    fn to_p4(&self, header: &Header) -> anyhow::Result<Vec<u8>> {
+        let mut out = Vec::new();
+        let line_byte_offset = 4 - (header.width / 8);
+        for line_bytes in &self.bitmap {
             let line_slice = line_bytes.get(line_byte_offset..4).context("slice")?;
 
             #[cfg(debug_assertions)]
@@ -87,9 +114,8 @@ impl Glyph {
                 println!();
             }
 
-            bitmap.extend_from_slice(&line_slice);
+            out.extend_from_slice(&line_slice);
         }
-
-        Ok(Glyph { encoding, bitmap })
+        Ok(out)
     }
 }
